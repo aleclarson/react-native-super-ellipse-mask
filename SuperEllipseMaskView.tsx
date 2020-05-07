6bacd2badc5283b828632075c5412645f36a977c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-05-07 16:56:19 -0400

    fix: use forwardRef

--- SuperEllipseMaskView.tsx
+++ SuperEllipseMaskView.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { requireNativeComponent, ViewProps } from 'react-native'
+import { requireNativeComponent, ViewProps, NativeMethodsMixin } from 'react-native'
 
 const SuperEllipseMask = requireNativeComponent('SuperEllipseMask')
 
-export const SuperEllipseMaskView = (props: ViewProps)=>
-  <SuperEllipseMask {...props} />
+export const SuperEllipseMaskView = React.forwardRef(
+  (props: ViewProps, ref: React.Ref<NativeMethodsMixin>) =>
+    <SuperEllipseMask ref={ref} {...props} />
+)
 
 SuperEllipseMaskView.displayName = 'SuperEllipseMaskView'
